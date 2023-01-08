25bfd9d32a965924a2814f4662bbb4fb0bb62d7f
“yauheni-kryzhyk-deriv” <yauheni@deriv.me>
2023-01-08 21:17:08 +0300

    store tests

--- src/__tests__/PaymentMethod.store.test.tsx
+++ src/__tests__/PaymentMethod.store.test.tsx
@@ -27,25 +27,36 @@ describe("Store", () => {
   });
 
   it("Should update the selected country", () => {
-    expect(true).toBeFalsy();
+    store.updateSelectedCountry({
+      text: 'test text',
+      value: 'test value',
+      phone_idd: 'test phone',
+    })
+    expect(store.selectedCountry.text).toBe('test text');
   });
 
   it("Should have initial select country value on resetSelectedCountry", () => {
-    expect(true).toBeFalsy();
+    store.resetSelectedCountry();
+    expect(store.selectedCountry.text).toBe('');
   });
 
   it("Should update payment methods", () => {
-    expect(true).toBeFalsy();
+    expect(store.paymentMethods.length).toBe(0)
+    store.updatePaymentMethods(fake_payment_methods.payment_methods)
+    expect(store.paymentMethods.length).toBe(1);
   });
 
   it("Should clear payment methods on resetPaymentMethods", () => {
-    expect(true).toBeFalsy();
+    store.updatePaymentMethods(fake_payment_methods.payment_methods)
+    expect(store.paymentMethods.length).toBe(1);
+    store.resetPaymentMethods();
+    expect(store.paymentMethods.length).toBe(0);
+
   });
 
-  it("Should have loading as fulsy by default", () => {
-    expect(true).toBeFalsy();
-  });
   it("Should have loading as truthy on toggleLoading", () => {
-    expect(true).toBeFalsy();
+    expect(store.loading).toBeFalsy();
+    store.toggleLoading();
+    expect(store.loading).toBeTruthy();
   });
 });
